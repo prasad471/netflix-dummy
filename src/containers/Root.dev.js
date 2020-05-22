53a69ec205f04abf0e627bc90f645024e9ed8615
Bobbili <801269@cognizant.com>
2020-05-22 14:01:56 +0530

    dev and production changes

---
+++ src/containers/Root.dev.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+import store, { persiststore } from "../store";
+import DevTools from "./DevTools";
+import StyledContainer from "../styled/StyledContainer";
+import { ThemeProvider } from "styled-components";
+import theme from "../constants/theme";
+import App from "../components/App";
+import { BrowserRouter as Router } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+
+const Root = () => {
+  return (
+    <Provider store={store}>
+      <PersistGate persistor={persiststore} loading={null}>
+      <ThemeProvider theme={theme}>
+        <StyledContainer>
+          <Router>
+            <App />
+          </Router>
+          <DevTools />
+          <ToastContainer />
+        </StyledContainer>
+      </ThemeProvider>
+      </PersistGate>
+   
+    </Provider>
+  );
+};
+
+export default Root;
--- src/containers/Root.js
+++ src/containers/Root.js
@@ -1,34 +1,5 @@
-import React from "react";
-import { Provider } from "react-redux";
-import { PersistGate } from "redux-persist/integration/react";
-import store, { persiststore } from "../store";
-import DevTools from "./DevTools";
-import StyledContainer from "../styled/StyledContainer";
-import { ThemeProvider } from "styled-components";
-import theme from "../constants/theme";
-import App from "../components/App";
-import { BrowserRouter as Router } from "react-router-dom";
-import { ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
-
-
-const Root = () => {
-  return (
-    <Provider store={store}>
-      <PersistGate persistor={persiststore} loading={null}>
-      <ThemeProvider theme={theme}>
-        <StyledContainer>
-          <Router>
-            <App />
-          </Router>
-          <DevTools />
-          <ToastContainer />
-        </StyledContainer>
-      </ThemeProvider>
-      </PersistGate>
-   
-    </Provider>
-  );
-};
-
-export default Root;
+if (process.env.NODE_ENV === "production") {
+  module.exports = require("./Root.prod");
+} else {
+  module.exports = require("./Root.dev");
+}
